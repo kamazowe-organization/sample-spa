36601ac1478b4aefc1bc313734d71751d93bfd35
Stasiek99 <stasiekpilich@gmail.com>
2022-04-23 13:36:29 +0200

    moved js to the bottom of index.ts file replaced var to let

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,43 @@ console.log('hello, world');
 const testMessage: string = 'TypeScript works';
 
 console.log(testMessage);
+
+let los = Math.floor((Math.random() * 100) + 1);
+let liczba;
+let ile=0;
+console.log;
+
+do {
+    liczba = parseInt(prompt("Podaj liczbe !"));
+    ile =ile+1
+
+    if(liczba>los){
+        console.log("To za duzo")
+    }
+    else if(liczba<los){
+        console.log("To za malo")
+    }
+    else if(liczba == los){
+        console.log("Wygraleś")
+        let dec = prompt("Czy chcesz zagrac jeszcze raz ? T/N");
+        if(dec == "T"){
+            ile=0;
+            continue;
+        }
+        else{
+            break;
+        }
+    }
+
+    if(ile==20 && liczba != los){
+        console.log("Przegrales")
+        let dec = prompt("Czy chcesz zagrac jeszcze raz ? T/N");
+        if(dec == "T"){
+            ile=0;
+            continue;
+        }
+        else{
+            break;
+        }
+    }
+}while (liczba!=los)
